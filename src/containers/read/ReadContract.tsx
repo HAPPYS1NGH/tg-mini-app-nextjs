5408e637447d86453559e8ef8b57c24112c382d6
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2024-04-11 16:12:37 +0530

    chore: updated the frontend to fetch from stylus

--- src/containers/read/ReadContract.tsx
+++ src/containers/read/ReadContract.tsx
@@ -1,52 +1,25 @@
 "use client"
-import { useReadContract, useAccount } from "wagmi"
+import { useReadContract } from "wagmi"
+
+import { counterAbi } from "@/constants/abi"
+import { counterAddress } from "@/constants"
 
 export function ReadContract() {
-  const { address } = useAccount()
   const {
-    data: balance,
+    data: counter,
     status,
     isLoading,
     error,
   } = useReadContract({
-    abi: [
-      {
-        constant: false,
-        inputs: [
-          {
-            name: "num",
-            type: "uint256",
-          },
-        ],
-        name: "store",
-        outputs: [],
-        payable: false,
-        stateMutability: "nonpayable",
-        type: "function",
-      },
-      {
-        constant: true,
-        inputs: [],
-        name: "retrieve",
-        outputs: [
-          {
-            name: "",
-            type: "uint256",
-          },
-        ],
-        payable: false,
-        stateMutability: "view",
-        type: "function",
-      },
-    ],
-    address: "0x3e9C748E9DBB864Ee4dE65FA16343Cde878DF7D0",
-    functionName: "retrieve",
+    abi: counterAbi,
+    address: counterAddress,
+    functionName: "number",
   })
-  console.log(balance, status, isLoading, error)
+  console.log(counter, status, isLoading, error)
 
   return (
     <div>
-      {isLoading ? <div>Loading</div> : <p>Balance: {balance?.toString()}</p>}
+      {isLoading ? <div>Loading</div> : <p>Counter: {counter?.toString()}</p>}
     </div>
   )
 }
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -33,8 +33,8 @@ export const config = getDefaultConfig({
     },
   ],
   chains: [
-    arbitrum,
     arbitrumStylus,
+    arbitrum,
     arbitrumSepolia,
     arbitrumNova,
     arbitrumGoerli,
