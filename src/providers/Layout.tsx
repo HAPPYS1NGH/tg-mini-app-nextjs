4507a9a27d7b50646387aae79b1697e7ae7b5e6e
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2024-07-19 02:49:26 +0530

    chore: error boundary

--- src/providers/Layout.tsx
+++ src/providers/Layout.tsx
@@ -1,15 +1,47 @@
-import React from "react"
-import Navbar from "@/components/shared/Navbar"
-import { Toaster } from "@/components/ui/sonner"
+"use client";
+import React, { useEffect } from "react";
+import Navbar from "@/components/shared/Navbar";
+import { Toaster } from "@/components/ui/sonner";
+import {
+  useBackButton,
+  useClosingBehavior,
+  useViewport,
+} from "@telegram-apps/sdk-react";
+import { useRouter, usePathname } from "next/navigation";
 
 function Layout({ children }: { children: React.ReactNode }) {
+  const bb = useBackButton();
+  const close = useClosingBehavior(); // will be undefined or ClosingBehavior.
+  const viewport = useViewport(); // will be undefined or InitData.
+  const router = useRouter();
+  const pathname = usePathname();
+  useEffect(() => {
+    function goBack() {
+      router.back();
+    }
+    if (close) {
+      close.enableConfirmation();
+    }
+    if (viewport) {
+      viewport.expand();
+    }
+    if (bb) {
+      if (pathname === "/") {
+        bb.hide();
+        return;
+      }
+      bb.show();
+      bb.on("click", goBack);
+    }
+  }, [bb, router, pathname]);
+
   return (
     <>
       <Navbar />
-      <main className="mx-12 my-8 ">{children}</main>
+      <main className=" mx-8 my-4 ">{children}</main>
       <Toaster richColors />
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
