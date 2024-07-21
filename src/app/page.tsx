2b9c72142d304dd344749b89e9a402447eef101e
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2024-07-22 02:51:04 +0530

    feat: ui done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,46 @@
-"use client"
+"use client";
 
-// import { Image } from "lucide-react"
-import Link from "next/link"
+import { useUtils } from "@telegram-apps/sdk-react";
+import Link from "next/link";
 
 export default function Home() {
+  const utils = useUtils();
   return (
-    <main className="p-24">
-      <h1 className="text-center text-3xl p-20 font-bold lg:mb-32 ">
-        Welcome to Arbitrum Starter Kit
-        {/* <Image src='/arbitrum.svg' className="w-20 h-20 inline-block" /> */}
+    <main className="">
+      <h1 className="text-center text-2xl p-5 font-bold  ">
+        Welcome to Mini App Starter Kit
       </h1>
-      <div className="grid xl:grid-cols-4 grid-cols-1 lg:grid-cols-2 space-x-3 ">
-        <div className="p-4 mx-4 hover:bg-gray-100 hover:border-b-2 rounded-lg">
+      <div className="grid xl:grid-cols-4 grid-cols-1 lg:grid-cols-2  gap-4 ">
+        <div className="p-4 mx-4 hover:bg-gray-200 w-fit bg-gray-100 hover:border-b-2 rounded-lg">
           <Link
             href="https://docs.arbitrum.io/welcome/get-started"
             target="_blank"
           >
-            <h1 className="text-2xl font-bold">Arbitrum -&gt;</h1>
+            <h1 className="text-xl font-bold">Arbitrum -&gt;</h1>
             <p className="text-sm text-gray-700">
               Learn about the formidable Layer 2 and how to develop
             </p>
           </Link>
         </div>
-        <div className="p-4 mx-4 hover:bg-gray-100 hover:border-b-2 rounded-lg">
+        <div className="p-4 mx-4 hover:bg-gray-200 w-fit bg-gray-100 hover:border-b-2 rounded-lg">
           <Link href="https://wagmi.sh/react/getting-started" target="_blank">
-            <h1 className="text-2xl font-bold">Wagmi -&gt;</h1>
+            <h1 className="text-xl font-bold">Wagmi -&gt;</h1>
             <p className="text-sm text-gray-700">
               Type Safe, Extensible, and Modular by design.
             </p>
           </Link>
         </div>{" "}
-        <div className="p-4 mx-4 hover:bg-gray-100 hover:border-b-2 rounded-lg">
+        <div className="p-4 mx-4 hover:bg-gray-200 w-fit bg-gray-100 hover:border-b-2 rounded-lg">
           <Link href="https://ui.shadcn.com/docs" target="_blank">
-            <h1 className="text-2xl font-bold">Shadcn -&gt;</h1>
+            <h1 className="text-xl font-bold">Shadcn -&gt;</h1>
             <p className="text-sm text-gray-700">
-              Beautifully designed components that you can copy and paste into
-              your apps.
+              Beautifully designed components that you use in your apps.
             </p>
           </Link>
         </div>{" "}
-        <div className="p-4 mx-4 hover:bg-gray-100 hover:border-b-2 rounded-lg">
+        <div className="p-4 mx-4 hover:bg-gray-200 w-fit bg-gray-100 hover:border-b-2 rounded-lg">
           <Link href="https://arbitrum-faucet.vercel.app/" target="_blank">
-            <h1 className="text-2xl font-bold">Faucet -&gt;</h1>
+            <h1 className="text-xl font-bold">Faucet -&gt;</h1>
             <p className="text-sm text-gray-700">
               Get the faucets to start deploying the app to the actual testnet.
             </p>
@@ -49,5 +48,5 @@ export default function Home() {
         </div>
       </div>
     </main>
-  )
+  );
 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,1 @@
-export const counterAddress = "0x3e9C748E9DBB864Ee4dE65FA16343Cde878DF7D0"
+export const counterAddress = "0x75b167a1628b6F239C0BEA211d731578bA90465a"
