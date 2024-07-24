f7514dc3ae0dc2cbaf62fdaf3b5e0ccc7a200b39
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2024-07-24 18:25:06 +0530

    feat: font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,21 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Outfit, Be_Vietnam_Pro } from "next/font/google";
 import "@rainbow-me/rainbowkit/styles.css";
 import "./globals.css";
 import Web3Provider from "@/providers/Web3Provider";
 import Layout from "@/providers/Layout";
 import { TelegramProvider } from "@/providers/TelegramProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+const outfit = Outfit({ subsets: ["latin"], variable: "--font-outfit" });
+const beVietnamPro = Be_Vietnam_Pro({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-beVietnamPro",
+});
 
 export const metadata: Metadata = {
-  title: "Create Arbitrum DApp",
-  description: "Template for creating a DApp on Arbitrum",
+  title: "Create ETH Mini App",
+  description: "Template for creating a Mini App on Ethereum",
 };
 
 export default function RootLayout({
@@ -19,8 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" className={`${outfit.variable} ${beVietnamPro.variable}`}>
+      <body className={beVietnamPro.className}>
         <Web3Provider>
           <TelegramProvider>
             <Layout>{children}</Layout>
